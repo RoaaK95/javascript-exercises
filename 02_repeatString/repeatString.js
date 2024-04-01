e1805faffa7a963b094a63217523393e474cf2f0
Roaa K <ruaakhuldoon.26@gmail.com>
2024-04-01 23:19:38 +0300

    SOLVE 01_helloWorld

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+   if(times<0) return "ERROR";
+   let string= "";
+   
+   for(let i=0;i<times;i++)
+   {
+    string+=text;
+   }
+   return string;
 };
 
+repeatString("hey",3);
 // Do not edit below this line
 module.exports = repeatString;
