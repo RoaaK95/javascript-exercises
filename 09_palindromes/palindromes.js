350ad7c58e336a70cd5233b640bfe8af2271a18e
Roaa K <ruaakhuldoon.26@gmail.com>
2024-04-28 07:37:35 +0300

    SOLVE 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const primeString= str
+                         .toLowerCase()
+                         .split('')
+                         .filter(char=> alphanumerical.includes(char))
+                         .join('');
+   
+   const reversedString= primeString.split('').reverse().join('');
+
+   return primeString===reversedString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
