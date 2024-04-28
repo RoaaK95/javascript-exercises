e078ad494d5c4bd67f3fee4aacc83695dc9ec9d5
Roaa K <ruaakhuldoon.26@gmail.com>
2024-04-28 07:55:52 +0300

    SOLVE 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+   let number;
+   if(typeof num !== 'number')
+   {
+    number= parseInt(num);
+   }
+   else 
+   {
+    number = num;
+   }
+   if(number<0) return "OOPS";
+   
+   const fibArray=[0,1];
+   for(i=2;i<=number;i++)
+   {
+      fibArray[i]=fibArray[i-2]+fibArray[i-1];
+   }
+   return fibArray[number];
 };
 
 // Do not edit below this line
